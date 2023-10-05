3af409f3b55ea663e228b3c014b26814a59a7ef0
Titus Moore <titusdmoore@gmail.com>
2023-10-05 18:55:04 -0400

    init commit. Added auth provider and prisma, still need to add user model, just scaffolding auth

---
+++ app/middleware.ts
@@ -0,0 +1,17 @@
+import { withAuth } from "next-auth/middleware";
+
+// middleware is applied to all routes, use conditionals to select
+
+export default withAuth(
+  function middleware (_req) {},
+  {
+    callbacks: {
+      authorized: ({ req, token }) => {
+        if ( req.nextUrl.pathname.startsWith('/app') && token === null ) {
+          return false
+        }
+        return true
+      }
+    }
+  }
+);
