f870e9923a17fe4ce73fdaebd452f4c5445f054c
Titus Moore <titusdmoore@gmail.com>
2023-10-07 00:24:20 -0400

    Added more data structure, added types util

--- app/tasks/new/newTaskForm.tsx
+++ app/tasks/new/newTaskForm.tsx
@@ -3,9 +3,16 @@
 import { useForm, Resolver } from "react-hook-form";
 import { useRouter } from "next/navigation";
 
+import { TaskStatus, Priority } from "@/utils/types";
+
 type FormValues = {
   title: string;
   content: string;
+  priority: Priority;
+  status: TaskStatus;
+  startDate: Date;
+  endDate: Date;
+  asignees: any[];
 };
 
 export default function NewTaskForm() {
---
+++ utils/types.ts
@@ -0,0 +1,13 @@
+export enum Priority {
+  LOW = "LOW",
+  MEDIUM = "MEDIUM",
+  HIGH = "HIGH",
+  CRITICAL = "CRITICAL",
+};
+
+export enum TaskStatus {
+  ACTIVE = "ACTIVE",
+  BACKLOG = "BACKLOG",
+  INPROGRESS = "INPROGRESS",
+  COMPLETED = "COMPLETED",
+}
