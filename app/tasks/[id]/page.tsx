d3131ef6bf1e8f93fe3beac20c639fa9c1a22ff2
Titus Moore <titusdmoore@gmail.com>
2023-10-12 18:33:53 -0400

    Added page

---
+++ app/tasks/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page(props) {
+  return (
+    <main>
+      <div className="card bg-white h-full shadow-xl p-4 mx-16">
+        <div className="overflow-x-auto text-black">
+        </div>
+      </div>
+    </main>
+  );
+}
